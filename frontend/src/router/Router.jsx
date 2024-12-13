55a9aeea84c7417ccc465a09e4a079d0327a227c
iva.miksa <iva.miksa@gmail.com>
2024-12-13 18:09:05 +0100

    layout added

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -1,34 +1,35 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Protector from "../pages/authenticated/Protector";
 import NotFound from "../pages/public/NotFound";
 import HomePage from "../pages/public/HomePage";
 import LoginPage from "../pages/authenticated/LoginPage";
 import RegisterPage from "../pages/authenticated/RegisterPage";
 import RegisterValidationPage from "../pages/authenticated/RegisterValidationPage";
+import Layout from "../components/Layout";
 
-
-const Router = () =>  {
+const Router = () => {
   return (
     <>
-    <Routes>
-       {/* Authentication and Registration Routes */}
+      <Routes>
+        <Route element={<Layout />}>
+          {/* Authentication and Registration Routes */}
 
-       {/* Public routes */}
-       <Route path="/" element={<HomePage/>}/>
-       <Route path="*" element={<NotFound/>}/>
-       <Route path="/login" element={<LoginPage />} />
-       <Route path="/register" element={<RegisterPage />} />
-       <Route path="/registervalidation" element={<RegisterValidationPage />} />
+          {/* Public routes */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route
+            path="/registervalidation"
+            element={<RegisterValidationPage />}
+          />
 
-       {/* Protected routes */}
-       <Route element={<Protector/>}>
-
-       </Route>
-
-    </Routes>
-      
+          {/* Protected routes */}
+          <Route element={<Protector />}></Route>
+        </Route>
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
