eb6ff4f7606dd036b8d7644c809b0fd9eba1b018
iva.miksa <iva.miksa@gmail.com>
2025-01-23 15:03:06 +0100

    Footer.jsx added

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,13 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
-    <div>
-      <Header />
-      <main>
+    <div className="flex flex-col min-h-screen">
+
+      <Header className="h-16 flex-none" />
+
+      <main className="flex-grow">
         <Outlet />
       </main>
+
+      <Footer className="h-16 flex-none" />
     </div>
   );
 };
