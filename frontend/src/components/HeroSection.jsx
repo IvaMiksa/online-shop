1e0492ff25b067c3153822dc1785accd9bbe1613
iva.miksa <iva.miksa@gmail.com>
2025-01-23 15:10:33 +0100

    hero section added

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,31 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+const HeroSection = () => {
+  return (
+    <section className="bg-gradient-to-r from-palevioletred to-palevioletredhover text-white py-16 px-8">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+        <div className="md:w-1/2 space-y-4">
+          <h1 className="text-4xl md:text-6xl font-bold">
+            Welcome to your Dream Shop
+          </h1>
+          <p className="text-lg md:text-xl">
+            Discover the best deals, exclusive products, and much more. Shop
+            your favorites now!
+          </p>
+          <button className="px-6 py-3 bg-white text-palevioletred rounded-lg shadow-md hover:bg-gray-100 transition">
+            Shop now
+          </button>
+        </div>
+        <div className="md:w-1/2 mt-8 md:mt-0 flex justify-center">
+          <DotLottieReact
+            src="https://lottie.host/1c9d2eeb-a6d5-4b79-b9e0-f25e61485fdf/V6NUxeoYke.lottie"
+            loop
+            autoplay
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "regal-blue": "#243c5a",
+        antiquewhite: "rgb(250,235, 215)",
+        palevioletred: "rgb(216,112,147)",
+        palevioletredhover: "rgb(220, 141, 167)",
+        "palevioletred-light": "rgb(237, 186, 203)",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
